a3129d14396f5df78edf8d5b014eadd26f8c61f6
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-09-09 18:04:05 +0300

    2

--- zerno.js
+++ zerno.js
@@ -1,44 +1,44 @@
 function loadGrain(levels) {
-  let left = 0; //вказівник зліва
-  let right = levels.length - 1; //вказвик зправа
-  let resalt = 0;
-  let max_r = 0;
-  let max_l = 0;
+  let left = 0; //pointer left
+  let right = levels.length - 1; //pointer right
+  let result = 0;
+  let max_right = 0;
+  let max_left = 0;
 
-  let count = 0; //кількість ітерацій
+  let count = 0; //number of iterations
 
-  //якщо довжина levels дорівнює значенню менше ніж 3, кількість зерна в баржі дорівнюватиме 0
-  if (levels.length == 2 || levels.length == 0 || levels.length == 1) {
+  //if the length of levels is less than 3, the amount of grain in the barge will be 0
+  if (!Array.isArray(levels) || levels.length < 3) {
     return 0;
-  } else {
-    while (left < right) {
-      //
-      count++;
-      if (levels[left] < levels[right]) {
-        if (levels[left] > max_l) {
-          max_l = levels[left];
-        } else {
-          resalt += max_l - levels[left];
-        }
-        left++;
+  }
+
+  while (left < right) {
+    count++;
+    if (levels[left] < levels[right]) {
+      if (levels[left] > max_left) {
+        max_left = levels[left];
       } else {
-        if (levels[right] > max_r) {
-          max_r = levels[right];
-        } else {
-          resalt += max_r - levels[right];
-        }
-        right--;
+        result += max_left - levels[left];
       }
+      left++;
+    } else {
+      if (levels[right] > max_right) {
+        max_right = levels[right];
+      } else {
+        result += max_right - levels[right];
+      }
+      right--;
     }
   }
+
   //console.log(count);
-  return resalt;
+  return result;
 }
 
-console.log(loadGrain([0, 1, 0, 2, 1, 0, 1, 3]));
-// console.log(loadGrain([4, 1, 3]));
-// console.log(loadGrain([2, 1, 5, 2, 7, 4, 10]));
-// console.log(loadGrain([2, 0, 1, 5, 2, 7]));
-// console.log(loadGrain([2, 4, 2]));
-// console.log(loadGrain([7, 4]));
-// console.log(loadGrain([]));
+console.log(loadGrain([4, 1, 3]));
+console.log(loadGrain(3));
+console.log(loadGrain([2, 1, 5, 2, 7, 4, 10]));
+console.log(loadGrain([2, 0, 1, 5, 2, 7]));
+console.log(loadGrain([2, 4, 2]));
+console.log(loadGrain([7, 4]));
+console.log(loadGrain([]));
